636403b06180de9b8c266c03838c5abb6601d731
atf29 <atf29@cam.ac.uk>
2017-10-11 09:54:51 +0100

    utility to split a GeneralInputParser randomly into 2 sets by fraction of the number of structures included

---
+++ util/split_sets.py
@@ -0,0 +1,29 @@
+"""
+routines to generate training and test sets
+"""
+import numpy as np
+import parsers
+
+def seperate_gips(gip,train_fraction):
+    """
+    split a single parsers.GeneralInputParser() into
+    test and train parsers.GeneralInputParser()'s
+    """
+    num_structures = len(gip.supercells)
+
+    train = parsers.GeneralInputParser()
+    test = parsers.GeneralInputParser()
+
+    # pick randomly
+    train_idx = np.random.choice(np.arange(num_structures),size=round(train_frac*num_structures),replace=False)
+    test_idx = set(np.arange(num_structures)).difference(set(train_idx))
+
+    for ii,_s in enumerate(gip.supercells):
+        if ii in train_idx:
+            train.supercells.append(_s)
+        elif ii in test_idx:
+            test.supercells.append(_s)
+
+    assert len(test.supercells)+len(train.supercells)==num_structures,'error in test/train splitting'           
+
+    return {"train":train,"test":test}
