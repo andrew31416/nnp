d332b9d97d77097a27658ac7994c2a95f03d68b6
atf29 <atf29@cam.ac.uk>
2017-10-11 09:04:48 +0100

    routine to create vector of vdw radii for given structures not included

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
 from nn_potential.features import *
+from nn_potential.observables import *
 
 __version__ = '1.0.0'
---
+++ observables/sedc.py
@@ -0,0 +1,12 @@
+"""
+observables associated with semi-empirical dispersion which one
+may want to regress
+"""
+import numpy as np
+
+def vdw_radius(gips):
+    obs = np.asarray([],dtype=np.float64)
+    for _gip in gips:
+        for _s in _gip:
+            obs = np.hstack((obs, _s["vdw_radius"]["environment"] - _s["vdw_radius"]["vacuum"] ))
+    return obs
