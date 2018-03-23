00af82d384a9eac45ebc7a7ce44b157352497eb3
atf29 <atf29@cam.ac.uk>
2018-03-23 17:34:15 +0000

    default Behler features changed to be consistent with unusual Amp feature defaults

--- features/defaults.py
+++ features/defaults.py
@@ -5,16 +5,16 @@ def Behler(train_data):
     defaults = nnp.features.types.features(train_data=train_data,PCA=None)
 
     rcut = 6.5
-    rs = 1.0
+    rs = 0.0
 
     for _eta in [0.05,4.0,20.0,80.0]:
         defaults.add_feature(nnp.features.types.feature("acsf_behler-g2",{"rcut":rcut,\
-                "fs":0.1,"eta":_eta,"rs":rs,"za":1.0,"zb":1.0}))
+                "fs":0.1,"eta":_eta/rcut**2,"rs":rs,"za":1.0,"zb":1.0}))
 
     for _lambda in [-1.0,1.0]:
         for _xi in [1.0,4.0]:
             defaults.add_feature(nnp.features.types.feature("acsf_behler-g4",\
-                    {"rcut":rcut,"fs":0.1,"eta":0.005,"xi":_xi,"lambda":_lambda,\
+                    {"rcut":rcut,"fs":0.1,"eta":0.005/rcut**2,"xi":_xi,"lambda":_lambda,\
                     "za":1.0,"zb":1.0}))
 
     return defaults            
